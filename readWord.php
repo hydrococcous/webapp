90d182a1b733270d2f360977873ec72c4ce0172d
sandrobirke <sandro.birke@gmail.com>
2014-08-04 16:50:29 +0200

    Read docx
    entpacken und XML-Ausgabe in Funktion eingebunden

--- readWord.php
+++ readWord.php
@@ -4,31 +4,37 @@
      * Quelle: http://www.jackreichert.com/2012/11/09/how-to-convert-docx-to-html/
      * Quelle: http://webcheatsheet.com/php/reading_the_clean_text_from_docx_odt.php
      */
-
+    
+    /**
+     * Quell-Datei
+     */
     $xmlFile = "results/brief_beispiel_04-08-14-14-36-52.docx";
     
     /**
      * DOCX entpacken
      */
-    
-    $zip = new ZipArchive;
-    if($zip->open($xmlFile) === true){
-        
-        $index =  $zip->locateName("word/document.xml");
-        
-        if($index !== false){
-            
-            $data = $zip->getFromIndex($index);
-            $zip->close();
-            
+    function unzipDOCX($file){
+        $zip = new ZipArchive;
+        if($zip->open($file) === true){
+
+            $index =  $zip->locateName("word/document.xml");
+
+            if($index !== false){
+
+                $data = $zip->getFromIndex($index);
+                $zip->close();
+
+            }
+
+           return $data;
+
+        } else {
+            return 'Fehler beim öffnen!';
         }
-        
-       print_r($data);
-        
-    } else {
-        echo 'Fehler beim öffnen!';
     }
     
+    $xmlString = unzipDOCX($xmlFile);
+    echo $xmlString;
     
     
     
