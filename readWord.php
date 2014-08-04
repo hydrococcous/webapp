3164baca1a7193435c02b4598c119cf5543f6884
sandrobirke <sandro.birke@gmail.com>
2014-08-04 23:21:23 +0200

    readWord
    XML aus docx extrahiert und al temporäres File gespeichert. Weiter
    Vorgehensweise -> XML mittels PHP auslesen und in HTML-Struktur
    darstellen

--- readWord.php
+++ readWord.php
@@ -1,44 +1,47 @@
 <?php
-    
-    /**
-     * Quelle: http://www.jackreichert.com/2012/11/09/how-to-convert-docx-to-html/
-     * Quelle: http://webcheatsheet.com/php/reading_the_clean_text_from_docx_odt.php
-     */
-    
-    /**
-     * Quell-Datei
-     */
-    $xmlFile = "results/brief_beispiel_04-08-14-14-36-52.docx";
-    
-    /**
-     * DOCX entpacken
-     */
-    function unzipDOCX($file){
+    // http://blogs.msdn.com/b/brian_jones/archive/2005/07/05/intro-to-word-xml-part-1-simple-word-document.aspx
+
+    function docx2text($filename) {
+        return readZippedXML($filename, "word/document.xml");
+    }
+
+    function readZippedXML($archiveFile, $dataFile) {
+        // Create new ZIP archive
         $zip = new ZipArchive;
-        
-        if($zip->open($file) === true){
 
-            $index =  $zip->locateName("word/document.xml");
-
-            if($index !== false){
-
+        // Open received archive file
+        if (true === $zip->open($archiveFile)) {
+            // If done, search for the data file in the archive
+            if (($index = $zip->locateName($dataFile)) !== false) {
+                // If found, read it to the string
                 $data = $zip->getFromIndex($index);
+                $xml = $zip->getFromName("word/document.xml");
+                // Close archive file
                 $zip->close();
-
+                // Load XML from a string
+                // Skip errors and warnings
+                # $xml = DOMDocument::loadXML($data, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
+                // Return data without XML formatting tags
+                return $xml;
+                }
+            $zip->close();
             }
 
-           return $data;
-
-        } else {
-            return 'Fehler beim öffnen!';
+            // In case of failure return empty string
+            return "Fehler";
         }
-    }
+        
+        
+    $xmlStr = docx2text("results/brief_beispiel_04-08-14-20-56-00.docx"); // Save this contents to file
     
-    $xmlString = unzipDOCX($xmlFile);
-    echo $xmlString;
+    $tmpFile = tempnam('results/','tmp_');
+    $handle = fopen($tmpFile,'w');
+    fwrite($handle, $xmlStr);
+    fclose($handle);
+    rename($tmpFile,$tmpFile.'.xml');
+    
+    $xml = simplexml_load_file($tmpFile.'.xml');
+    var_dump($xml);
     
     
-    
-
-
 ?>
