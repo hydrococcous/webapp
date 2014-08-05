fc88d5c6798afe85b61cfaf533f5ae03ed0f4eba
sandrobirke <sandro.birke@gmail.com>
2014-08-05 16:07:37 +0200

    readWord
    Ausgabe als HTML

--- readWord.php
+++ readWord.php
@@ -1,5 +1,6 @@
 <?php
     // http://blogs.msdn.com/b/brian_jones/archive/2005/07/05/intro-to-word-xml-part-1-simple-word-document.aspx
+    // http://www.data2type.de/xml-xslt-xslfo/wordml/wordml-einfuehrung/inzeilige-elemente/
 
     function docx2text($filename) {
         return readZippedXML($filename, "word/document.xml");
@@ -32,26 +33,72 @@
         }
         
         
-    $xmlStr = docx2text("results/brief_beispiel_04-08-14-20-56-00.docx"); // Save this contents to file
-    
-//    $tmpFile = tempnam('results/','tmp_');
-//    $handle = fopen($tmpFile,'w');
-//    fwrite($handle, $xmlStr);
-//    fclose($handle);
-//    rename($tmpFile,$tmpFile.'.xml');
-//    
-//    $xml = simplexml_load_file($tmpFile.'.xml');
-//    var_dump($xml);
-    
+    $xmlStr = docx2text("results/brief_beispiel_04-08-14-20-56-00.docx");
 
-$p = xml_parser_create();
-xml_parse_into_struct($p, $xmlStr, $vals, $index);
-xml_parser_free($p);
-echo '<pre>';
-echo "Index array\n";
-print_r($index);
-echo "\nVals array\n";
-print_r($vals);
-echo '</pre>';
+    $parser = xml_parser_create();
+    xml_parse_into_struct($parser, $xmlStr, $values, $index);
+    xml_parser_free($parser);
+
+    $html = '';
+    $format = false;
+
+    function openFormat($fmt){
+        switch($fmt){
+
+            case "W:B";
+                return '<strong>';
+                break;
+            default:
+                return false;
+        }
+    }
+
+    foreach($values as $value){
+
+        // öffnenden Paragraf finden
+        if($value['tag'] == 'W:P' && $value['type'] == 'open'){
+            $html .= '<p>';
+        }
+
+        // Format öffnen
+        if($value['tag'] == 'W:B' && $value['type'] == 'complete' && $format === false){
+            $html .= openFormat($value['tag']);
+            $closeTag = '</strong>';
+            $format = true;
+        }
+
+        // Textinhalt finden und schreiben
+        if($value['tag'] == 'W:T'){
+
+            // Prüfen ob tatsächlich ein Textinhalt existiert
+            if(array_key_exists('value',$value)){
+                $html .= $value['value'];
+                } else {
+                    $html .= '';
+                }
+
+            // Format schließen
+            if($format === true){
+                $html .= $closeTag;
+                $format = false;
+            }
+        }
+
+        // Line-Brak finden
+        if($value['tag'] == 'W:P' && $value['type'] == 'close'){
+            $html .= '</p>'."\n";
+        }
+
+        // schließenden Paragraf finden
+        if($value['tag'] == 'W:BR' && $value['type'] == 'complete'){
+            $html .= '</br>'."\n";
+        }   
+
+        #echo '<pre>';
+        #print_r($value);
+        #echo '</pre><hr />';  
+    }
+
+    echo $html;
     
 ?>
